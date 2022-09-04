679917eec9184926a1e88456b2ff140f1c9e3589
Liam Boudraa <liam.boudraa@etudiant.univ-reims.fr>
2022-09-04 19:15:59 +0200

    Feat - EasyAdmin Dashboard & Article Crud

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -3,7 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleCrudController extends AbstractCrudController
 {
@@ -12,14 +19,29 @@ class ArticleCrudController extends AbstractCrudController
         return Article::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
-            TextEditorField::new('description'),
+
+            SlugField::new('slug')
+                ->setTargetFieldName('title'),
+
+            TextEditorField::new('content'),
+
+            TextField::new('abstract'),
+
+            DateTimeField::new('createdAt')
+                ->hideOnForm(),
+
+            DateTimeField::new('updatedAt')
+                ->hideOnForm(),
         ];
     }
-    */
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle('new', 'Ajouter un %entity_name%');
+    }
 }
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,45 +2,55 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Article;
+use App\Entity\Category;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
 
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
+        return $this->redirect(
+            $adminUrlGenerator->setController(ArticleCrudController::class)->generateUrl()
+        );
 
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Symfony Forum');
+            ->setTitle('FoRhum Dashboard')
+            ->setTitle('<img alt="Logo FoRhum" style="height: 40px; margin-right: 5px" src="images/favicon-svg.svg">
+                             <span style="font-weight: 700">FoRhum</span>')
+            ->setFaviconPath('images/favicon-svg.svg');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        return [
+            MenuItem::linktoDashboard('Dashboard', 'fa fa-home'),
+
+            MenuItem::subMenu('Blog', 'fa fa-newspaper')->setSubItems([
+                MenuItem::linkToCrud('Liste des articles', 'fa fa-newspaper', Article::class),
+                MenuItem::linkToCrud('Ajouter un article', 'fa fa-pen', Article::class)
+                    ->setAction('new'),
+                MenuItem::linkToCrud('Catégories', 'fa fa-list', Category::class),
+            ]),
+
+            MenuItem::section(),
+            MenuItem::linkToRoute('Retour au site', 'fa fa-door-open', 'app_home'),
+//            MenuItem::linkToLogout('Logout', 'fa fa-exit'),
+        ];
+
     }
 }
