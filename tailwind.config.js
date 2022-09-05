87bec1abf708746fd11e5c01031afcd6a534778d
Liam Boudraa <liam.boudraa@etudiant.univ-reims.fr>
2022-09-05 23:23:10 +0200

    Feat - Flowbite typography install

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     plugins: [
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
-        require('flowbite/plugin')
+        require('flowbite/plugin'),
+        require('flowbite-typography'),
     ],
 }
