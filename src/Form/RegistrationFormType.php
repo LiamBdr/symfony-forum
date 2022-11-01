9d4dcac3943dd3b85dcd9b4b29e3eae1c10f3824
Liam Boudraa <liam.boudraa@etudiant.univ-reims.fr>
2022-11-01 19:44:41 +0100

    Feat - Inscription Form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -23,24 +23,33 @@ class RegistrationFormType extends AbstractType
             ->add('username', TextType::class, [
                 'label' => 'Pseudonyme',
                 'attr' => [
-                    'placeholder' => 'Pseudonyme',
+                    'placeholder' => 'Votre nom d\'utilisateur',
+                    'autocomplete' => 'username',
+                    'aria-autocomplete' => 'list',
                 ],
                 'help' => 'Votre pseudonyme sera visible par tous les utilisateurs.',
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Email',
                 'attr' => [
-                    'placeholder' => 'nom@email.fr',
+                    'placeholder' => 'votreadresse@exemple.com',
+                    'autocomplete' => 'email',
+                    'aria-describedby' => 'emailHelp',
+                    'aria-autocomplete' => 'list',
                 ],
             ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
                 'invalid_message' => 'The password fields must match.',
-                'options' => ['attr' => ['class' => 'password-field']],
+                'options' => ['attr' => [
+                    'class' => 'password-field',
+                    'autocomplete' => 'new-password',
+                    'aria-autocomplete' => 'list',
+                    'placeholder' => 'Votre mot de passe',
+                ]],
                 'required' => true,
-                'first_options'  => ['label' => 'Mot de passe'],
+                'first_options' => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Répéter le mot de passe'],
                 'empty_data' => 'Mot de passe',
                 'constraints' => [
@@ -61,8 +70,7 @@ class RegistrationFormType extends AbstractType
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
